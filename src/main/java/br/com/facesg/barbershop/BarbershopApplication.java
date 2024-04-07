37e240b9fdbf73d90870ea3e11134096c0ccb091
Yan Hermanny <88159884+yanhermanny@users.noreply.github.com>
2024-04-06 23:18:56 -0300

    First commit

---
+++ src/main/java/br/com/facesg/barbershop/BarbershopApplication.java
@@ -0,0 +1,12 @@
+package br.com.facesg.barbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/facesg/barbershop/BarbershopApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.facesg.barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
